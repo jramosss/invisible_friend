4f144a34d24e6453fa6d63544c448a9fdcb1b1ac
jramosss <jramostod@gmail.com>
2022-05-18 17:38:08 -0300

    A bit better

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+from routers.groups import groups
 
 app = Flask(__name__)
+app.register_blueprint(groups)
+
+
+@app.route('/')
+def greet():
+    return render_template('greet.html')
--- flaskr/groups.py
+++
@@ -1,3 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('groups', __name__, url_prefix='/groups')
