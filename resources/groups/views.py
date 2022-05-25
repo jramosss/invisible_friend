d4af0cd7c857a20df72cd78b61700018ad20e466
jramosss <jramostod@gmail.com>
2022-05-25 17:49:06 -0300

    Group creation

--- resources/groups/views.py
+++ resources/groups/views.py
@@ -8,25 +8,36 @@ groups = Blueprint('groups', __name__,
 
 @groups.route('/')
 def index():
-    print(Group)
     groups = Group.query.all()
     users = Person.query.all()
-    print(groups)
     return render_template('index.html', groups=groups, users=users)
 
 
-@groups.route('/register/', methods=['POST'])
+@groups.route('/create', methods=["GET", 'POST'])
+def create():
+    if request.method == "POST":
+        name = request.form['name']
+        group = Group(name=name)
+        db.session.add(group)
+        db.session.commit()
+        return redirect(url_for('groups.index'))
+    return render_template("group_create.html")
+
+
+@groups.route('/register', methods=["GET", "POST"])
 def add_to_group():
-    name = request.form['name']
-    email = request.form['email']
-    group_id = request.form['group_id']
-    if not Group.query.filter_by(id=group_id).exists():
-        abort(500)
-    if not name:
-        return '<p>Please enter a name</p>'
-    if not email:
-        return '<p>Please enter an email</p>'
-    new_person = Person(name=name, email=email, group_id=group_id)
-    db.session.add(new_person)
-    db.session.commit()
-    return redirect(url_for("groups.register"))
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        group_id = request.form['group_id']
+        if not Group.query.filter_by(id=group_id).exists():
+            abort(500)
+        if not name:
+            return '<p>Please enter a name</p>'
+        if not email:
+            return '<p>Please enter an email</p>'
+        new_person = Person(name=name, email=email, group_id=group_id)
+        db.session.add(new_person)
+        db.session.commit()
+    else:
+        return render_template("group_register.html")
