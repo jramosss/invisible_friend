d0276fe6aa8b6660c3428d4f330c3014617a3c04
jramosss <jramostod@gmail.com>
2022-05-30 00:59:33 -0300

    Removed unneccessary stuff

--- resources/groups/views.py
+++ resources/groups/views.py
@@ -1,6 +1,5 @@
 from app import db
 from flask import Blueprint, abort, redirect, render_template, request, url_for
-from resources.groups.utils import get_groups
 
 from .models import Group, Person, Profile
 
@@ -10,8 +9,7 @@ groups = Blueprint('groups', __name__,
 
 @groups.route('/')
 def index():
-    groups = get_groups()
-    print(groups)
+    groups = Group.query.all()
     return render_template('index.html', groups=groups)
 
 
@@ -22,43 +20,22 @@ def create():
         group = Group(name=name)
         db.session.add(group)
         db.session.commit()
-        return redirect(url_for('groups.edit', group_id=group.id))
+        return redirect(url_for('groups.update', group_id=group.id))
     return render_template("group_create.html")
 
 
 @groups.route('/edit/<int:group_id>', methods=["GET", 'POST'])
-def edit(group_id: int):
+def update(group_id: int):
     group = Group.query.get_or_404(group_id)
     if request.method == "POST":
         new_user_name = request.form['user_name']
         new_user_email = request.form['user_email']
         if new_user_email in [profile.person.email for profile in group.members]:
             abort(400, "Email already in group")
+        # TODO get or create
         new_user = Person(name=new_user_name, email=new_user_email)
         new_profile = Profile(person=new_user, group=group)
         db.session.add_all([new_user, new_profile])
         db.session.commit()
         group = Group.query.get_or_404(group_id)
     return render_template("group.html", group=group)
-
-
-@groups.route('/register', methods=["GET", "POST"])
-def add_to_group():
-    if request.method == "POST":
-        name = request.form['name']
-        email = request.form['email']
-        group_id = request.form['group_id']
-        if not Group.query.filter_by(id=group_id).exists():
-            abort(500)
-        if not name:
-            return '<p>Please enter a name</p>'
-        if not email:
-            return '<p>Please enter an email</p>'
-        person = Person.query.filter_by(email=email).first()
-        group = Group.query.filter_by(group_id=group_id).first()
-        new_profile = Profile(person=person, group=group)
-        db.session.add(new_profile)
-        db.session.commit()
-        return redirect(url_for("groups.register"))
-    else:
-        return render_template("group_register.html")
